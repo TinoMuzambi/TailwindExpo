8d8dfe5a786933a67ca907a015d604a93718343a
Tino Muzambi <tinomuzambi@gmail.com>
2021-12-25 22:37:01 +0200

    Updates meta.

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -41,19 +41,12 @@ const Meta: React.FC<MetaProps> = ({
 
 		<meta charSet="utf-8" />
 		<link rel="icon" href="/favicon.svg" />
-		<meta name="VARSITY Newspaper" content="VARSITY Newspaper" />
+		<meta name={title} content={title} />
 		<link rel="apple-touch-icon" href="/logo192.png" />
 		<link rel="manifest" href="/manifest.json" />
 		<title>{title}</title>
 
 		<link rel="preconnect" href="https://a.storyblok.com" />
-		<link rel="preconnect" href="https://www.google-analytics.com" />
-		<link
-			rel="stylesheet"
-			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-			integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-			crossOrigin="anonymous"
-		/>
 	</Head>
 );
 
@@ -62,7 +55,7 @@ Meta.defaultProps = {
 	keywords: "tailwind, tailwindcss, css, typescript, nextjs, react, reactjs",
 	description:
 		"My first dive into using Tailwind CSS. This is a simple landing page with a hero section, a cards section, contact form and a footer, fully responsive. This is a Next.js site with TypeScript.",
-	image: "https://a.storyblok.com/f/114267/512x512/9abcfc4334/logo512.png",
+	image: "https://a.storyblok.com/f/105639/512x512/03489159d5/logo512.png",
 	url: "",
 };
 
