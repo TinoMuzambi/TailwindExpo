f623a5f806bead1515cebb337ff2259bad4dbe56
Tino Muzambi <tinomuzambi@gmail.com>
2021-12-25 16:30:54 +0200

    Adds and styles nav.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,25 @@ import Head from "next/head";
 
 export default function Home() {
 	return (
-		<div className="">
+		<>
 			<Head>
 				<title>Expo</title>
 			</Head>
 
+			<header>
+				<nav className="flex items-center justify-between p-4 bg-teal-500 text-white">
+					<p className="font-bold text-xl">TinoMuzambi</p>
+					<ul className="flex gap-4">
+						<li className="link">About</li>
+						<li className="link">Projects</li>
+						<li className="link">Socials</li>
+					</ul>
+				</nav>
+			</header>
+
 			<main className=""></main>
 
 			<footer className=""></footer>
-		</div>
+		</>
 	);
 }
